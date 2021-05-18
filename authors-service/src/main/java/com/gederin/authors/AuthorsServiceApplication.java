4b505ae488faf977c9006117e65124556eaaab4e
rgederin <gederin.ruslan@gmail.com>
2021-05-18 09:27:48 +0300

    initial commits

---
+++ authors-service/src/main/java/com/gederin/authors/AuthorsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gederin.authors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorsServiceApplication.class, args);
+    }
+
+}
